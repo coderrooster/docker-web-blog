c4f389701f3c667bf6e322f7f646467663c39b28
Coder Rooster <abitohari@gmail.com>
2021-01-26 14:37:24 +0700

    create php file

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Learn Docker - Web 1</title>
+  <style>
+    .box{
+      height: 50px;
+      width: 50px;
+      border: 1px solid #ddd;
+      border-radius: 5px;
+      padding: 5px;
+      margin: 10px;
+    }
+  </style>
+</head>
+<body>
+  <h3>Belajar Docker - Web 1</h3>
+  <div class="box">
+    <h3>Hai Ini Box 1</h3>
+  </div>
+  <div class="box">
+    <h3>Hai Ini Box 2</h3>
+  </div>
+  <div class="box">
+    <h3>Hai Ini Box 3</h3>
+  </div>
+</body>
+</html>
